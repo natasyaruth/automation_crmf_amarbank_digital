aac35939ec33cfaf2f49ecce377625c1a91edf55
Fajar Ardhi Haqi <fajar.ardhi@amarbank.co.id>
2022-08-12 16:31:26 +0700

    Website [Add Test Case T105146, T105147, T105148, T105149, T105150]

---
+++ Object Repository/Website/CRM/CSR_Management/Details/AlertRefPhoneNumber.rs
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>AlertRefPhoneNumber</name>
+   <tag></tag>
+   <elementGuidId>1d722e0c-2617-49a6-8869-16f91e8de0a1</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.u-fs-small.u-clr-danger.u-mg-top-xxsmall</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Nomor kontak kerabat'])[1]/following::div[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>773599b4-9f4a-413b-b340-25b8e84a899e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>u-fs-small u-clr-danger u-mg-top-xxsmall</value>
+      <webElementGuid>bc7f0420-b1f7-474d-9ea3-7f8c846134c7</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Nomor kontak kerabat wajib diisi</value>
+      <webElementGuid>c8c4c304-8a81-4d20-89dc-05ecce8f08fc</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;PersonalDataForm&quot;)/form[@class=&quot;Form&quot;]/div[@class=&quot;Grids&quot;]/div[@class=&quot;Grid&quot;]/div[@class=&quot;Form-field Form-field--danger&quot;]/div[@class=&quot;u-fs-small u-clr-danger u-mg-top-xxsmall&quot;]</value>
+      <webElementGuid>5df4e3a0-ac56-4c52-8bba-b5b7c44c8f43</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='PersonalDataForm']/form/div/div/div[3]/div[2]</value>
+      <webElementGuid>71c2f845-8f6a-4a09-8cae-60aaa52fa89a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Nomor kontak kerabat'])[1]/following::div[2]</value>
+      <webElementGuid>5bca9a4d-a5f9-4229-80c4-6f1e281858d5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Nama ibu kandung'])[1]/following::div[4]</value>
+      <webElementGuid>b7f1ee6e-ab45-4359-8cf5-ecae4b90d714</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='NPWP (optional)'])[1]/preceding::div[1]</value>
+      <webElementGuid>e1c4568b-5c88-4a6d-a41d-e8e0e2983c36</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Agama'])[2]/preceding::div[3]</value>
+      <webElementGuid>a08c3c06-c0a2-42c7-b115-eaa96da2fc35</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Nomor kontak kerabat wajib diisi']/parent::*</value>
+      <webElementGuid>52669321-e609-41d4-950f-689251764dd7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/div[3]/div[2]</value>
+      <webElementGuid>0c04ff9b-a715-4053-963e-e31e4ecaf4de</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Nomor kontak kerabat wajib diisi' or . = 'Nomor kontak kerabat wajib diisi')]</value>
+      <webElementGuid>a64b3d37-c1a4-4bdf-9c70-a420dceae182</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
