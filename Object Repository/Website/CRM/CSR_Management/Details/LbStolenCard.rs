2814b93f4293256be11f045d4fdda49e62b7c3f4
Fajar Ardhi Haqi <fajar.ardhi@amarbank.co.id>
2022-09-04 16:55:14 +0700

    Website [Add TestCase T105477 , T105478, T105479]

---
+++ Object Repository/Website/CRM/CSR_Management/Details/LbStolenCard.rs
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>LbStolenCard</name>
+   <tag></tag>
+   <elementGuidId>9e5f39ac-e3eb-4ed5-a185-3d9e05754b0a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Alasan permintaan kartu baru'])[1]/following::label[2]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>label</value>
+      <webElementGuid>0cad47c5-e3fe-4e37-b663-75853786fb78</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>for</name>
+      <type>Main</type>
+      <value>RbStolenCard</value>
+      <webElementGuid>1fd78797-7c25-468f-98a8-97355dab3ac5</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Kartu dicuri</value>
+      <webElementGuid>d6a13911-12b7-467c-9761-04f29982cd80</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;outer-root&quot;)/div[@class=&quot;Modal is-opened&quot;]/div[@class=&quot;Modal-dialog RequestNewCardModal&quot;]/div[@class=&quot;u-MarginBottom-large&quot;]/form[1]/div[@class=&quot;Radio u-FontSize-large u-MarginBottom-small&quot;]/label[1]</value>
+      <webElementGuid>bf4eaf40-5a6e-4f68-9e6e-7d76f30eaf67</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='outer-root']/div/div/div[2]/form/div[2]/label</value>
+      <webElementGuid>d76b4d20-428d-4c07-8dd9-22db45012c35</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Kartu hilang'])[1]/following::label[1]</value>
+      <webElementGuid>995d69ae-5121-4ef3-9082-5a068d65a610</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Alasan permintaan kartu baru'])[1]/following::label[2]</value>
+      <webElementGuid>8d84bb2e-36d4-4e3b-afe7-8bcf5ccfc393</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Tutup kartu'])[1]/preceding::label[1]</value>
+      <webElementGuid>2c9ebf3a-1337-436d-9a5a-6ffede5869ab</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Permintaan Kartu Baru'])[3]/preceding::label[2]</value>
+      <webElementGuid>a474d0cc-4992-4df9-9ec8-f8a0db2c1f1e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Kartu dicuri']/parent::*</value>
+      <webElementGuid>cdf7fec5-1a1a-46c9-9699-5b349059e0bd</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/form/div[2]/label</value>
+      <webElementGuid>e4ffa22e-4139-43a0-a200-bcce6f721b22</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//label[(text() = 'Kartu dicuri' or . = 'Kartu dicuri')]</value>
+      <webElementGuid>76b050b8-9a52-413f-953d-6ab044d9f6e7</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
