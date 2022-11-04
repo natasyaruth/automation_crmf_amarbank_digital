9ff57568ad2ec589add3cbfadd3830132068e89c
Fajar Ardhi Haqi <fajar.ardhi@amarbank.co.id>
2022-11-04 18:17:38 +0700

    Website [added back office T 1350145 1dayregression]

---
+++ Scripts/URL/URL_Staging_CRM_Back_Office/Script1667292803128.groovy
@@ -0,0 +1,32 @@
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import com.kms.katalon.util.CryptoUtil
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.configuration.RunConfiguration as RunConfiguration
+
+/* Set up list value set web driver preferences property*/
+RunConfiguration.setWebDriverPreferencesProperty('args', listValue)
+RunConfiguration.setWebDriverPreferencesProperty('prefs', listOfPrefs)
+
+/* Set input username & password basic auth on browser web page */
+WebUI.openBrowser((((('https://' + CryptoUtil.decode(CryptoUtil.getDefault(GlobalVariable.authUserBackOffice))) + ':') + CryptoUtil.decode(CryptoUtil.getDefault(GlobalVariable.authPassBackOffice))) + '@') + GlobalVariable.siteUrlBackOff.substring(
+        8))
+/* Set window is maximize*/
+WebUI.maximizeWindow()
+
+/* Set navigate to setup URL */
+WebUI.navigateToUrl(GlobalVariable.siteUrlBackOff)
