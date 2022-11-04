1ee0876cc1dc1afa2741a52a4d9f41e31f775e4c
Ruth Hutauruk <ruth.hutauruk@amarbank.co.id>
2022-11-04 09:55:51 +0700

    Website[Create automation CSR Detail for KTP Image, Card, Change log and other content]

---
+++ Object Repository/Website/CRM/CSR_Management/Details/LbColumnPhoto.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>LbColumnPhoto</name>
+   <tag></tag>
+   <elementGuidId>4624f47c-a3b9-480c-9f71-0c5818c6d48d</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
