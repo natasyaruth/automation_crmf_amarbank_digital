38dd362b1b8a9bff21a757544542bc40031953b7
Michael William <michael.william@amarbank.co.id>
2022-11-25 16:19:13 +0700

    Website [T1350010, T1350068, T1350153]

---
+++ Object Repository/Website/CRM/KYC_Management/KYC_Verification/Details/BtnFaceMatchDukcapil.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>BtnFaceMatchDukcapil</name>
+   <tag></tag>
+   <elementGuidId>ce9b38a3-8db8-450e-97d0-709d88ca0295</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@id='BtnFacematchDukcapil']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Website/CRM/KYC_Management/KYC_Verification/RejectionReason/ChkChangeEmail.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ChkChangeEmail</name>
+   <tag></tag>
+   <elementGuidId>972982bf-3452-4a9d-8941-c702d2320b2c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@id='ChkChangeEmail']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
