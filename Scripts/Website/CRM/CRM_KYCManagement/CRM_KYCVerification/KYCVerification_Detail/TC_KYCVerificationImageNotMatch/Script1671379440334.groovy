4f9b89787cd14380927d6c0322f4c4126bc615c7
Fajar Ardhi Haqi <fajar.ardhi@amarbank.co.id>
2022-12-19 14:35:44 +0700

    Website [Adding testcase T1877816]

--- Scripts/Website/CRM/CRM_KYCManagement/CRM_KYCVerification/KYCVerification_Detail/TC_KYCVerificationImageNotMatch/Script1671379440334.groovy
+++ Scripts/Website/CRM/CRM_KYCManagement/CRM_KYCVerification/KYCVerification_Detail/TC_KYCVerificationImageNotMatch/Script1671379440334.groovy
@@ -44,6 +44,7 @@ KeywordUtil keylogger = new KeywordUtil()
 	- Button "Cek" Facematch Verihub is enable & FM Dukcapil is disable
 	- Value is record on changes log and CSR
  */
+'Noted for this cases currently I just catch the existing data with these condition and also I see the next API process if any provide it will use with that'
 
 'Init access HTML'
 WebDriver driverKycVerif = DriverFactory.getWebDriver()
@@ -54,17 +55,105 @@ List<WebElement> colsKycVerif
 'We want access to KYC Verification'
 if (WebUI.waitForElementVisible(linkMenuKycManagement, 5)) {
 	WebUI.click(linkMenuKycManagement)
+	WebUI.delay(5)
+	WebUI.waitForPageLoad(5)
 	if (WebUI.waitForElementVisible(linkMenuKycVerification, 5)) {
 		WebUI.click(linkMenuKycVerification)
+		WebUI.delay(5)
+		WebUI.waitForPageLoad(5)
 		tblKycVerif = driverKycVerif.findElement(By.xpath('//table/tbody'))
 		rowsKycVerif = tblKycVerif.findElements(By.tagName('tr'))
 		colsKycVerif = rowsKycVerif.get(0).findElements(By.tagName('td'))
-		if (colsKycVerif.get(7).getText().equalsIgnoreCase('Sedang diproses')) {
+		if (colsKycVerif.get(4).getText().equalsIgnoreCase('Nasabah Baru')) {
 			colsKycVerif.get(7).findElement(By.xpath('a')).click()
 			TestObject accessInKycVerifDetail = new TestObject().addProperty('text',ConditionType.CONTAINS,'KYC Customer Detail')
 			if (WebUI.waitForElementVisible(accessInKycVerifDetail, 5)) {
 				WebUI.click(btnBackToKyc)
-			}
+			} else {keylogger.markError('We are not in KYC Detail')}
+		} else {keylogger.logInfo('We can continue the process')}
+	} else {keylogger.markError('ELement not visible please check again')}
+} else {keylogger.markError('Element not visible please check again')}
+
+'We want access the request ID'
+WebUI.verifyOptionsPresent(drpDwnCustType, listDrpDwnCustType)
+WebUI.selectOptionByLabel(drpDwnCustType, 'Nasabah Baru', false)
+WebUI.setText(txtReqIdKycVerif, reqIdWithImageNotMatch)
+WebUI.sendKeys(txtReqIdKycVerif, Keys.chord(Keys.ENTER))
+
+tblKycVerif = driverKycVerif.findElement(By.xpath('//table/tbody'))
+rowsKycVerif = tblKycVerif.findElements(By.tagName('tr'))
+colsKycVerif = rowsKycVerif.get(0).findElements(By.tagName('td'))
+if (colsKycVerif.get(1).getText().equalsIgnoreCase(reqIdWithImageNotMatch)) {
+	colsKycVerif.get(4).getText().equalsIgnoreCase('Nasabah Baru')
+	colsKycVerif.get(7).findElement(By.xpath('a')).click()
+} else {keylogger.markError('Request ID not match')}
+TestObject accessKycCustDetail = new TestObject().addProperty('text',ConditionType.CONTAINS,'KYC Customer Detail')
+if (WebUI.waitForElementPresent(accessKycCustDetail, 5)) {
+	WebUI.scrollToElement(txtImageNotFound, 5)
+	WebUI.verifyElementText(txtImageNotFound, "Image is not match")
+	WebUI.verifyElementNotClickable(btnFmDukcapil)
+	WebUI.verifyElementClickable(btnFmVerihub)
+	WebUI.scrollToElement(btnBackToKyc, 0)
+	WebUI.click(btnBackToKyc)
+} else {keylogger.markError('We are not in KYC Customer Detail')}
+
+'Init access HTML'
+WebDriver driverCsrMgt = DriverFactory.getWebDriver()
+WebElement tblCsrMgt
+List<WebElement> rowsCsrMgt
+List<WebElement> colsCsrMgt
+
+'We want check in CSR Management'
+if (WebUI.waitForElementVisible(linkMenuCsrManagement, 5)) {
+	WebUI.click(linkMenuCsrManagement)
+	if (WebUI.waitForElementVisible(alertNotif, 5)) {
+		WebUI.click(btnAbort)
+	} else {keylogger.logInfo('We can continue the process')}
+	WebUI.setText(txtReqIdCsrMgt, reqIdWithImageNotMatch)
+	WebUI.sendKeys(txtReqIdCsrMgt, Keys.chord(Keys.ENTER))
+	tblCsrMgt = driverCsrMgt.findElement(By.xpath('//table/tbody'))
+	rowsCsrMgt = tblCsrMgt.findElements(By.tagName('tr'))
+	colsCsrMgt = rowsCsrMgt.get(0).findElements(By.tagName('td'))
+	if (colsCsrMgt.get(5).getText().equalsIgnoreCase('Nasabah Baru')) {
+		colsCsrMgt.get(6).findElement(By.xpath('button')).click()
+		TestObject accessToCustDetail = new TestObject().addProperty('text',ConditionType.CONTAINS,'Customer Detail')
+		if (WebUI.waitForElementPresent(accessToCustDetail, 5)) {
+			keylogger.markPassed('We are success to access in customer detail')
+			WebUI.takeScreenshot()
+		} else {keylogger.markError('We are not access Customer Detail')}
+	} else {keylogger.markError('We are not get the nasabah baru')}
+} else {keylogger.markError('Element not visible')}
+
+'Init access HTML'
+WebDriver driverCsrMgtDtl = DriverFactory.getWebDriver()
+WebElement tblCsrMgtDtl
+List<WebElement> rowsCsrMgtDtl
+List<WebElement> colsCsrMgtDtl
+
+'We want check on changelog CSR Detail'
+WebUI.scrollToElement(btnDataChangeLog, 5)
+WebUI.click(btnDataChangeLog)
+checkLoop = false
+fmDukcapil:
+while (checkLoop == false) {
+	WebUI.verifyOptionsPresent(drpDwnFilterBySource, listDrpDwnFilterBySource)
+	WebUI.selectOptionByLabel(drpDwnFilterBySource, 'KYC Verification', false)
+	tblCsrMgtDtl = driverCsrMgtDtl.findElement(By.xpath('//*[@id="changelog"]//table/tbody'))
+	rowsCsrMgtDtl = tblCsrMgtDtl.findElements(By.tagName('tr'))
+	for (int i; i < rowsCsrMgtDtl.size() ; i++) {
+		colsCsrMgtDtl = rowsCsrMgtDtl.get(i).findElements(By.tagName('td'))
+		if (colsCsrMgtDtl.get(2).getText().equalsIgnoreCase('KYC Verification')) {
+			colsCsrMgtDtl.get(3).getText().equalsIgnoreCase('Facematch Dukcapil')
+			colsCsrMgtDtl.get(5).getText().equalsIgnoreCase('48.7%')
+			colsCsrMgtDtl.get(6).getText().equalsIgnoreCase('Melakukan face match')
+			keylogger.markPassed('We already to check Face Match Dukcapil')
+			WebUI.scrollToElement(btnBackCsrBucket, 5)
+			WebUI.click(btnBackCsrBucket)
+			break fmDukcapil
+		} else {
+			keylogger.logInfo('We must check in another row')
+			tblCsrMgtDtl = driverCsrMgtDtl.findElement(By.xpath('//*[@id="changelog"]//table/tbody'))
+			rowsCsrMgtDtl = tblCsrMgtDtl.findElements(By.tagName('tr'))
 		}
 	}
 }
