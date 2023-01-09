2098e2fdbfb1d195bc777aa0415853312772a7c3
Tryse Rezza <tryse.rezza@amarbank.co.id>
2023-01-09 07:27:55 +0700

    WEBSITE[Fixing Head on BtnLiveness]

--- Object Repository/Website/CRM/KYC_Management/KYC_Verification/Details/BtnLiveness.rs
+++ Object Repository/Website/CRM/KYC_Management/KYC_Verification/Details/BtnLiveness.rs
@@ -3,7 +3,6 @@
    <description></description>
    <name>BtnLiveness</name>
    <tag></tag>
-<<<<<<< HEAD
    <elementGuidId>da96bfe4-e244-4235-be1a-7030e5f4a636</elementGuidId>
    <selectorCollection>
       <entry>
@@ -21,19 +20,4 @@
       <value>BtnLiveness</value>
       <webElementGuid>2d304415-2a62-48ac-8269-7346b92f4b0c</webElementGuid>
    </webElementProperties>
-=======
-   <elementGuidId>1606145e-acc7-491e-a7e3-9307e5c4bb30</elementGuidId>
-   <selectorCollection>
-      <entry>
-         <key>BASIC</key>
-         <value></value>
-      </entry>
-      <entry>
-         <key>XPATH</key>
-         <value>//button[@id='BtnLiveness']</value>
-      </entry>
-   </selectorCollection>
-   <selectorMethod>XPATH</selectorMethod>
-   <useRalativeImagePath>false</useRalativeImagePath>
->>>>>>> c5a75ff87eb817902009303550430cd93f25b6df
 </WebElementEntity>
