f3c7fe6db92a22c09294cdeaacd822226e7629ff
Tryse Rezza <tryse.rezza@amarbank.co.id>
2023-01-09 07:19:51 +0700

    WEBSITE[Fixing Head comment]

--- Object Repository/Website/CRM/KYC_Management/KYC_Verification/Bucketlist/DrpCustomerType.rs
+++ Object Repository/Website/CRM/KYC_Management/KYC_Verification/Bucketlist/DrpCustomerType.rs
@@ -3,7 +3,6 @@
    <description></description>
    <name>DrpCustomerType</name>
    <tag></tag>
-<<<<<<< HEAD
    <elementGuidId>24ce1ac7-a888-49d7-9d1c-485808e866a4</elementGuidId>
    <selectorCollection>
       <entry>
@@ -21,19 +20,4 @@
       <value>DrpCustomerType</value>
       <webElementGuid>50e72dcf-4b36-40c7-9833-a09d9842d431</webElementGuid>
    </webElementProperties>
-=======
-   <elementGuidId>9a534002-7475-4d8a-9732-0f35bfbd4412</elementGuidId>
-   <selectorCollection>
-      <entry>
-         <key>BASIC</key>
-         <value></value>
-      </entry>
-      <entry>
-         <key>XPATH</key>
-         <value>//select[@id='DrpCustomerType']</value>
-      </entry>
-   </selectorCollection>
-   <selectorMethod>XPATH</selectorMethod>
-   <useRalativeImagePath>false</useRalativeImagePath>
->>>>>>> c5a75ff87eb817902009303550430cd93f25b6df
 </WebElementEntity>
