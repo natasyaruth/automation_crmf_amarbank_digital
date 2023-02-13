2cf1eacceddf136468119a2bd5442c4192c665fd
Fajar Ardhi Haqi <fajar.ardhi@amarbank.co.id>
2023-02-13 15:45:21 +0700

    Website [Adding new branch CRMF-1200 & Update script TC_ClickAcceptForOriginEmbedBanking , TC_ClickCancelConfirmationCheckFMVerifhub]

--- Scripts/Website/CRM/CRM_KYCManagement/CRM_KYCVerification/KYCVerification_Detail/TC_ClickCancelInConfirmationCheckFacematchVerihub/Script1672921090432.groovy
+++ Scripts/Website/CRM/CRM_KYCManagement/CRM_KYCVerification/KYCVerification_Detail/TC_ClickCancelInConfirmationCheckFacematchVerihub/Script1672921090432.groovy
@@ -12,22 +12,38 @@ import com.kms.katalon.core.testdata.TestData as TestData
 import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
 import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.driver.DriverFactory
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
+import internal.GlobalVariable
+
+import org.openqa.selenium.By
+import org.openqa.selenium.Keys
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+
 import com.kms.katalon.core.util.KeywordUtil as KeywordUtil
 
 /*'Declaration keylog forloggin'*/
 KeywordUtil keyLogger = new KeywordUtil()
 
+/*Precondition
+ * This case condition is FM verihub must enable button
+ * */
+
 /*'We want to makesure we can access KYC Management'*/
 boolean checkMenuKYC = WebUI.verifyElementVisible(menuKYCManagement, FailureHandling.OPTIONAL)
 
 if (checkMenuKYC == true) {
+	
     WebUI.click(menuKYCManagement)
 
     WebUI.click(menuKYCVerification)
+	
+	WebUI.waitForPageLoad(5)
+	
+	WebUI.delay(3)
+	
 } else {
     keyLogger.markFailed('Something happen with menu KYC Management')
 }
@@ -35,27 +51,98 @@ if (checkMenuKYC == true) {
 /* Verify KYC Verification Bucketlist*/
 WebUI.verifyElementVisible(headerKYCVerificationElement)
 
-WebUI.selectOptionByLabel(drpEmailType, EmailisVerified, false)
+loopCheckData = false
+loopVerif:
+while (loopCheckData == false) {
+	
+	WebDriver driver = DriverFactory.getWebDriver()
+	
+	WebElement kycVerifTable = driver.findElement(By.xpath('//table/tbody'))
+	
+	List<WebElement> kycVerifRows = kycVerifTable.findElements(By.tagName('tr'))
+	
+	for (int i=0;i < kycVerifRows.size();i ++) {
+		
+		WebUI.selectOptionByLabel(drpEmailType, EmailisVerified, false)
+		
+		WebUI.selectOptionByLabel(drpCustomerType, NewCustomer, false)
+		
+		kycVerifTable = driver.findElement(By.xpath('//table/tbody'))
+		
+		kycVerifRows = kycVerifTable.findElements(By.tagName('tr'))
+		
+		List<WebElement> kycVerifColums = kycVerifRows.get(i).findElements(By.tagName('td'))
+		
+		if (kycVerifColums.get(4).getText().equalsIgnoreCase('Nasabah Baru')) {
+			
+			kycVerifColums.get(7).findElement(By.xpath('a')).click()
+			
+		} else {
+			
+			keyLogger.markFailed('We dont have data in bucket list')
+		}
+		
+		'This part I want check firtst the conditional'
+		if (WebUI.waitForElementClickable(btnFacematchDukcapil, 5)) {
+			
+			WebUI.click(btnFacematchDukcapil)
+			
+			if (WebUI.waitForElementVisible(btnConfirmationFacematch, 0)) {
+				
+				WebUI.click(btnConfirmationFacematch)
+				
+				WebUI.waitForPageLoad(5)
+				
+				WebUI.delay(3)
+				
+				break loopVerif
+				
+			}
+		} else {
+			
+			WebUI.click(btnBack)
+			
+			WebUI.waitForPageLoad(5)
+			
+			WebUI.delay(3)
+			
+			kycVerifTable = driver.findElement(By.xpath('//table/tbody'))
+			
+			kycVerifRows = kycVerifTable.findElements(By.tagName('tr'))
+			
+		}
+		
+	}
+	
+}
 
-WebUI.selectOptionByLabel(drpCustomerType, NewCustomer, false)
+if (WebUI.waitForElementClickable(btnFacematchVerihub, 5)) {
+	
+	WebUI.click(btnFacematchVerihub)
+	
+	if (WebUI.waitForElementVisible(btnCancelFacematch, 5)) {
+		
+		/* We want capture the result*/
+		WebUI.takeScreenshot()
+		
+		WebUI.click(btnCancelFacematch)
+			
+	} else {
+		
+		keyLogger.markFailed('Process is failed the condition is not correct')
+		
+	}
+	
+	
+} else {
+	
+	keyLogger.markFailed('Please to check the condition above , actualy the condition button FM liveness is disable')
+	
+}
 
-WebUI.click(btnRequestid)
-
-
-WebUI.click(btnFacematchDukcapil)
-
-WebUI.click(btnConfirmationFacematch)
-
-//WebUI.click(btnFacematchVerihub)
-//
-///* We want capture the result*/
-//WebUI.takeScreenshot()
-//
-//WebUI.click(btnCancelFacematch)
-
-WebUI.verifyElementNotClickable(btnFacematchVerihub)
 /* We want capture the result*/
 WebUI.takeScreenshot()
+
 /* We want to refresh for the next process*/
 WebUI.refresh()
 
